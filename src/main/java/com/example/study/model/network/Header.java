cff5207b61035bccd8ae968ff009972655424062
hohoprince <hoot0512@gmail.com>
2021-03-08 11:35:40 +0900

    사용자 조회 api 추가

---
+++ src/main/java/com/example/study/model/network/Header.java
@@ -0,0 +1,52 @@
+package com.example.study.model.network;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Header<T> {
+
+    private LocalDateTime transactionTime; // api 통신 시간
+
+    private String resultCode; // api 응답 코드
+
+    private String description; // api 부가 설명
+
+    private T data;  // data
+
+    // OK
+    public static <T> Header<T> OK() {
+        return (Header<T>) Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .build();
+    }
+
+    // DATA OK
+    public static <T> Header<T> OK(T data) {
+        return (Header<T>) Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .data(data)
+                .build();
+    }
+
+    // ERROR
+    public static <T> Header<T> ERROR(String description) {
+        return (Header<T>) Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("ERROR")
+                .description(description)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -0,0 +1,73 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.User;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        UserApiRequest userApiRequest = request.getData();
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.map(user -> response(user))
+        .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        // user -> userApiResponse
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // TODO: 암호화, 길이
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        // header + data return
+        return Header.OK(userApiResponse);
+    }
+
+}
