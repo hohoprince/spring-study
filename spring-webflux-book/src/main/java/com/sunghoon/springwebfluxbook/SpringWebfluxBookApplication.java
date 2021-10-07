fb64fa519c88750567f32f57af0b6576b16f1e26
hohoprince <hoot0512@gmail.com>
2021-10-07 19:32:41 +0900

    3장 devtools 사용법 추가

--- spring-webflux-book/src/main/java/com/sunghoon/springwebfluxbook/SpringWebfluxBookApplication.java
+++ spring-webflux-book/src/main/java/com/sunghoon/springwebfluxbook/SpringWebfluxBookApplication.java
@@ -2,11 +2,16 @@ package com.sunghoon.springwebfluxbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxBookApplication {
 
     public static void main(String[] args) {
+        BlockHound.builder()
+                .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
+                .install();
         SpringApplication.run(SpringWebfluxBookApplication.class, args);
     }
 
