35cab7642be3c5cf39b2d5aa742104753ede0061
hohoprince <hoot0512@gmail.com>
2021-10-07 15:24:48 +0900

    1장 간단한 리액티브 프로그래밍 실습

---
+++ spring-webflux-book/src/main/java/com/sunghoon/springwebfluxbook/SpringWebfluxBookApplication.java
@@ -0,0 +1,13 @@
+package com.sunghoon.springwebfluxbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxBookApplication.class, args);
+    }
+
+}
---
+++ spring-webflux-book/src/test/java/com/sunghoon/springwebfluxbook/SpringWebfluxBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunghoon.springwebfluxbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
