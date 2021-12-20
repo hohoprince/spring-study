f6c9274cfe33c69f81dba94b20265229f7b47526
hohoprince <hoot0512@gmail.com>
2021-12-20 14:58:21 +0900

    [spring mvc 2]로그인, 회원가입 페이지 추가

--- spring-mvc-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
+++ spring-mvc-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/validation/api/items")
